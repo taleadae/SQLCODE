ea31c4b0aee0af833eed085962c2276dc67614c3
taleadae <138217961+taleadae@users.noreply.github.com>
2023-07-23 18:23:41 -0400

    project1_design_a_store.sql

--- project1_design_a_store.sql
+++ project1_design_a_store.sql
@@ -1,23 +1,23 @@
---Create your own store! You should sell at least 15 items, and use select statements to order your items by price 
-and show at least one statistic about the items. -- 
+--Create your own store! You should sell at least 15 items, and use select statements to order your items by price --
+-- and show at least one statistic about the items. -- 
 
-CREATE TABLE SeussStore (id INTEGER, name TEXT, price INTEGER, quantity INTEGER, Stocked TEXT);
+CREATE TABLE SeussStore (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, price INTEGER, quantity INTEGER, Stocked TEXT);
 
-INSERT INTO SeussStore VALUES (1, 'One Fish', 4.25, 10, 'Y');
-INSERT INTO SeussStore VALUES (2, 'Two Fish', 4.25, 10, 'Y');
-INSERT INTO SeussStore VALUES (3, 'Red Fish', 4.25, 10, 'Y');
-INSERT INTO SeussStore VALUES (4, 'Blue Fish', 4.25, 10, 'Y');
-INSERT INTO SeussStore VALUES (5, 'Black Fish', 4.25, 2, 'Y');
-INSERT INTO SeussStore VALUES (6, 'Blue-er Fish', 4.50, 6, 'Y');
-INSERT INTO SeussStore VALUES (7, 'Old Fish', 3.25, 15, 'Y');
-INSERT INTO SeussStore VALUES (8, 'New Fish', 5.25, 20, 'Y');
-INSERT INTO SeussStore VALUES (9, 'Star Fish', 5.50, 10, 'Y');
-INSERT INTO SeussStore VALUES (10, 'Car Fish', 6.00, 0, 'N');
-INSERT INTO SeussStore VALUES (11, 'Sad Fish', 10.00, 10, 'N');
-INSERT INTO SeussStore VALUES (12, 'Glad Fish', 4.25, 13, 'N');
-INSERT INTO SeussStore VALUES (13, 'Bad Fish', 1.50, 0, 'N');
-INSERT INTO SeussStore VALUES (14, 'Thin Fish', 3.50, 5, 'N');
-INSERT INTO SeussStore VALUES (15, 'Fat Fish', 50.00, 3, 'Y');
+INSERT INTO SeussStore (name, price, quantity, stocked) VALUES ('One Fish', 4.25, 10, 'Y');
+INSERT INTO SeussStore (name, price, quantity, stocked) VALUES('Two Fish', 4.25, 10, 'Y');
+INSERT INTO SeussStore (name, price, quantity, stocked) VALUES('Red Fish', 4.25, 0, 'N');
+INSERT INTO SeussStore (name, price, quantity, stocked) VALUES('Blue Fish', 4.25, 10, 'Y');
+INSERT INTO SeussStore (name, price, quantity, stocked) VALUES('Black Fish', 4.25, 2, 'Y');
+INSERT INTO SeussStore (name, price, quantity, stocked) VALUES('Blue-er Fish', 4.50, 6, 'Y');
+INSERT INTO SeussStore (name, price, quantity, stocked) VALUES('Old Fish', 3.25, 15, 'Y');
+INSERT INTO SeussStore (name, price, quantity, stocked) VALUES('New Fish', 5.25, 20, 'Y');
+INSERT INTO SeussStore (name, price, quantity, stocked) VALUES('Star Fish', 5.50, 10, 'Y');
+INSERT INTO SeussStore (name, price, quantity, stocked) VALUES('Car Fish', 6.00, 0, 'N');
+INSERT INTO SeussStore (name, price, quantity, stocked) VALUES('Sad Fish', 10.00, 10, 'Y');
+INSERT INTO SeussStore (name, price, quantity, stocked) VALUES('Glad Fish', 4.25, 0, 'N');
+INSERT INTO SeussStore (name, price, quantity, stocked) VALUES('Bad Fish', 1.50, 0, 'N');
+INSERT INTO SeussStore (name, price, quantity, stocked) VALUES('Thin Fish', 3.50, 5, 'Y');
+INSERT INTO SeussStore (name, price, quantity, stocked) VALUES('Fat Fish', 50.00, 3, 'Y');
 
 SELECT * FROM SeussStore
 ORDER BY price;
